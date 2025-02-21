b4192871ce7cf61a9f12b204b3a6b822ed81a636
Gautam Kumar <gk62018@gmail.com>
2025-02-21 23:23:47 +0530

    [Gautam] Add . uc3 feature added

---
+++ src/main/java/com/tit/uc3/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.tit.uc3.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/")
+    public String sayHello() {
+        return "Hello from BridgeLabz";
+    }
+}
